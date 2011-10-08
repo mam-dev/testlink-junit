8d1f6cc672677220beb6e35f12caee0d30221539
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2011-10-08 21:21:43 +0200

    Add testcase for missing externalId and internalId in TestLink annotation.

--- tljunit/src/test/java/com/github/oneandone/testlinkjunit/tljunit/TestLinkRunListenerTest.java
+++ tljunit/src/test/java/com/github/oneandone/testlinkjunit/tljunit/TestLinkRunListenerTest.java
@@ -6,13 +6,16 @@ package com.github.oneandone.testlinkjunit.tljunit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
+import java.lang.annotation.Annotation;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.junit.Test;
+import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 
 import com.github.oneandone.testlinkjunit.tljunit.TestLinkRunListener.InTestLinkStrategy;
@@ -30,7 +33,7 @@ public class TestLinkRunListenerTest {
         final Xpp3Dom results = listener.getResults();
         assertEquals(6, results.getChildCount());
         int testCasesWithExternalId = 0;
-        for (final Xpp3Dom testCase: results.getChildren()) {
+        for (final Xpp3Dom testCase : results.getChildren()) {
             if (testCase.getAttribute("external_id") != null) {
                 testCasesWithExternalId++;
             }
@@ -53,5 +56,33 @@ public class TestLinkRunListenerTest {
         final InTestLinkStrategy inTestLinkStrategy = new TestLinkRunListener.InTestLinkStrategy();
         Xpp3Dom timeStamp = inTestLinkStrategy.createTimeStamp(calendar.getTime());
         assertEquals(XML_HEADER + "<timestamp>1970-01-01 01:00:00</timestamp>", timeStamp.toString());
+    }
+
+    @Test
+    public void testTestLinkAnnotationWithoutId() {
+        final Description description = Description.createSuiteDescription("foo", new TestLink() {
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return TestLink.class;
+            }
+
+            @Override
+            public long internalId() {
+                return 0;
+            }
+
+            @Override
+            public String externalId() {
+                return TestLink.NOT_AVAILABLE;
+            }
+        });
+        final InTestLinkStrategy inTestLinkStrategy = new TestLinkRunListener.InTestLinkStrategy();
+        try {
+            inTestLinkStrategy.addNewTestCase(description);
+            fail("IllegalArgumentException should be thrown!");
+        } catch (IllegalArgumentException e) {
+            assertEquals("java.lang.IllegalArgumentException: Must set either internalId or externalId on 'foo'", e.toString());
+        }
+
     }
 }
